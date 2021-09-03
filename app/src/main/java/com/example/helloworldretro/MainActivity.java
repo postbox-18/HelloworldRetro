6c1e9e5784575905de9dd640fbfe9cf4a0bfa899
aravinth <aravinth@aostasoftware.com>
2021-09-03 17:35:05 +0530

    shows in logcat

--- app/src/main/java/com/example/helloworldretro/MainActivity.java
+++ app/src/main/java/com/example/helloworldretro/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -16,18 +19,26 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     UserService userService;
     String TAG="user";
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textView=findViewById(R.id.textview);
         userService=Apiutils.getUserService();
+
+
         Call<ResponseBody> call = userService.login();
         call.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 try {
                     Log.e(TAG,"Response"+response.body().string());
+                    //String res=response.body().string();
+                    //JSONObject jsonObject=response.body().string();
+                    ///https://www.tutorialspoint.com/android/android_json_parser.htm
+
+                    textView.setText(response.body().string());
                     //1. string to json
                     /*2 . get key value of the json obj
                     3.set value to modle
